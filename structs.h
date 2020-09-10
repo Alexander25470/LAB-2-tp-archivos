8244699b64c653b467d02668acca06813425a9b4
Alexander25470 <61051951+Alexander25470@users.noreply.github.com>
2020-09-10 14:37:50 -0300

    Add files via upload

--- structs.h
+++ structs.h
@@ -1,3 +1,18 @@
 #pragma once
 
+struct Fecha{
+ int dia, mes, anio;
+};
 
+struct choferes{
+    char dni[10];
+    char apellido[50];
+    char nombre[50];
+    Fecha fechaIngreso;
+    char cuit[20];
+    int tipoReg;
+    Fecha vencimientoRegistro;
+    char telefono[15];
+    bool esPropietario;
+    bool estado;
+};
